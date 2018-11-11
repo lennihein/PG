b0f8b328c308c6d568c9aafc9a239b0366ea326c
Maurice <s6mahapp@uni-bonn.de>
2018-11-11 15:18:39 +0100

    added tracer (doesn't work yet)
    still need to find out how to insert the pid from dummy into tracer

---
+++ Week_1/article/Maurice/dummy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    int i;
+    for(i=0; i<10; i++)
+    {
+        //printf("My counter is %d\n",i);
+        sleep(2);
+    }
+    return 0;
+}
